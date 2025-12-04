4465617ab622649fcbec9550be0148d153ff83dc
Chaimaa Maach <maachechaimaa@gmail.com>
2025-12-04 16:06:48 +0100

    modif de fonction multiply

--- Groupe2/simple_calculator.py
+++ Groupe2/simple_calculator.py
@@ -3,5 +3,20 @@ def add(a,b):
 def subtract(a, b):
     return a-b
 def multiply(a, b):
-    return a*b
+    """Multiply using repeated addition"""
+    result = 0
+    negative = False
+    if b < 0:
+        b = -b
+        negative = True
+
+    for _ in range(int(b)):
+        result = add(result, a)
+
+
+    if negative:
+        result = -result
+
+
+
 
