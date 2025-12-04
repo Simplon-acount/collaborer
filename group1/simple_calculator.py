3141864eedf61574fc76d96eb9abd63e56defe7f
Assatir-dev <tonemail@gmail.com>
2025-12-04 15:10:14 +0100

    dev2

--- group1/simple_calculator.py
+++ group1/simple_calculator.py
@@ -3,3 +3,10 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Division par zéro interdite")
+    return a / b
