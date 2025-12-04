f1173d21d836b6afa4e36824689b61847da6b0c8
21269 <mawadaennaciri@gmail.com>
2025-12-04 15:46:18 +0100

    j'ajoute les deux fonction avergeMed(), moreDispersed()

--- group3/stat_calculator.py
+++ group3/stat_calculator.py
@@ -11,3 +11,24 @@ def normalise(df: pd.DataFrame):
 def mod(df1 : pd.DataFrame, df2 : pd.DataFrame):
     return df1.mode(), df2.mode()
 
+# 8. Moyenne et médiane
+def averageMed(df: pd.DataFrame):
+    avg = df.mean()
+    med = df.median()
+    return avg, med
+
+
+# 9. Dispersions après normalisation
+def moreDispersed(df1: pd.DataFrame, df2: pd.DataFrame):
+    n1 = normalise(df1)
+    n2 = normalise(df2)
+
+    disp1 = n1.std().mean()
+    disp2 = n2.std().mean()
+
+    if disp1 > disp2:
+        return "df1 est plus dispersé"
+    elif disp2 > disp1:
+        return "df2 est plus dispersé"
+    else:
+        return "Les deux DataFrames ont la même dispersion"
